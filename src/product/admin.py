77de468f07b4edf07567290ebe022713b163b00e
Facundo Varela <facundovarela88@hotmail.com>
2025-09-01 13:08:10 -0300

    Created Category model on Product APP

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.register(models.Category) #Me permite agregar un buscador y buscar un tipo de objeto por caracteres
+
--- src/product/models.py
+++ src/product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True, verbose_name='Description')
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Product categories'
+    
+    def validate_unique(self, exclude =None):
+        return super().validate_unique(exclude)
