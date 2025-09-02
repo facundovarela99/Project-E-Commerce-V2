354e88407856e47ffd6c1767ff8634e079f07866
Facundo Varela <facundovarela88@hotmail.com>
2025-09-02 14:09:01 -0300

    Adding validations to category form in forms.py

--- src/product/forms.py
+++ src/product/forms.py
@@ -5,3 +5,11 @@ class CategoryForm(forms.ModelForm): #Clase que nos permite trabajar los formula
     class Meta: #subclase Meta:sirve para configurar opciones adicionales que afectan cómo se comporta ese modelo.
         model = models.Category
         fields = ['name','description']
+    def clean_name(self):
+        name: str = self.cleaned_data.get('name', '')
+        if len(name) < 3:
+            raise forms.ValidationError('The name length it must be greater than 3')
+        if not name.isalpha():
+            raise forms.ValidationError('The name must not contain numbers')
+        return name
+
