6f9e9a4e56adbe707abb9716f85e6637c213f3a2
Facundo Varela <facundovarela88@hotmail.com>
2025-09-03 10:20:57 -0300

    db.sqlite3 removed of the .gitignore. Updating minimal lines

--- src/product/models.py
+++ src/product/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    #Products categories
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True, verbose_name='Description')
 
