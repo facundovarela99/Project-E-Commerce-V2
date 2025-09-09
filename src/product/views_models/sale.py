490a02969b8b68fe8ceb59ff277e673907f1f28b
Facundo Varela <facundovarela88@hotmail.com>
2025-09-09 10:29:46 -0300

    sale_create FBV, urls.py updated and sale_form.html template generated to create a new sale. Pending to make the logic so only the admin can update the sellers and the user himself just can make itself as a seller

--- src/product/views_models/sale.py
+++ src/product/views_models/sale.py
@@ -8,22 +8,30 @@ context = {'year':2025}
 def sale_list(request: HttpRequest) ->HttpResponse:
     search = request.GET.get('query')
     if search:
-        queryset = models.Sale.objects.filter(product__name__icontains=search)
+        queryset = models.Sale.objects.filter(
+            seller__user__username__icontains=search
+            ) | models.Sale.objects.filter(product__name__icontains=search)
     else:
         queryset = models.Sale.objects.all()
     context2 = context.copy()
     context2.update({'object_list':queryset})
     return render(request, 'product/Sale_crud/sale_list.html', context2)
 
-# # -------SALE - CREATE VIEW-------FUNCTION-BASED VIEW
-# def sale_create(request: HttpRequest) -> HttpResponse:
-#     if request.method == 'GET':
-#         form = forms.SaleForm()
-#     if request.method == 'POST':
-#         form = forms.SaleForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('product:sale_list')
-#     context2 = context.copy()
-#     context2.update({'form':form})
-#     return render(request, 'product/Sale_crud/sale_form.html')
+# -------SALE - CREATE VIEW-------FUNCTION-BASED VIEW
+def sale_create(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        form = forms.SaleForm()
+    if request.method == 'POST':
+        form = forms.SaleForm(request.POST)
+        if form.is_valid():
+            sale = form.save(commit=False)
+            product = form.cleaned_data['product']
+            amount = form.cleaned_data['amount']
+            sale.total_price = product.price * amount
+            sale.save()
+            return redirect('product:sale_list')
+    context2 = context.copy()
+    context2.update({'form':form})
+    return render(request, 'product/Sale_crud/sale_form.html', context2)
+#PENDIENTE: Que el usuario que esté logueado solo pueda crearse a sí mismo como vendedor, y solo el admin pueda administrar los vendedores
+#PENDIENTE: Que al generar una venta, el stock del producto vaya disminuyendo
